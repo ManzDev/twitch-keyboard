8d9d2e0b633bf370c220ea95457e77bac5c5ed3c
ManzDev <JoseRomanHernandez@gmail.com>
2021-05-31 20:38:03 +0100

    Twitch Javascript session: led, gamer leds, sounds, keystrokes

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const infoDiv = document.querySelector(".info");
+const sounds = [
+  new Audio("key1.mp3"),
+  new Audio("key2.mp3")
+];
+
+const keyPlay = () => {
+  const n = Math.random() * 1;
+  const i = n > 0.92 ? 1 : ~~n;
+  sounds[i].currentTime = 0;
+  sounds[i].play();
+};
+
+addEventListener("keydown", (ev) => {
+  const key = ev.code.toLowerCase();
+  const keyDiv = document.querySelector(`#${key}`);
+
+  if (key === "f4") {
+    document.querySelector(".keyboard").classList.toggle("led");
+  }
+
+  if ((key === "numlock") || (key === "capslock") || (key === "scrolllock")) {
+    const ledDiv = document.querySelector(`.led.${key}`);
+    ledDiv.classList.toggle("on");
+  }
+
+  // Pressed key
+  keyDiv.classList.add("pressed");
+  keyPlay();
+  infoDiv.textContent = `Se ha pulsado la tecla '${key}' ${ev.key} (${ev.keyCode})`;
+});
+
+addEventListener("keyup", (ev) => {
+  const key = ev.code.toLowerCase();
+  const keyDiv = document.querySelector(`#${key}`);
+  keyDiv.classList.remove("pressed");
+});
